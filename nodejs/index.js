fdfc27f7b342d6bbeac075e482bcee0c2ed99087
asadmukhtarr <axad03213@gmail.com>
2025-03-01 18:14:08 +0500

    products

--- nodejs/index.js
+++ nodejs/index.js
@@ -5,18 +5,18 @@ const path = require('path');
 const bodyparser = require("body-parser"); // form handling ...
 const url = "mongodb://localhost:27017";
 const client = new MongoClient(url);
-const db = "asadmukhtar";
 async function connectDB(){
     try {
        await client.connect();
        console.log("Connected With Server");
-       const db = client.db(db);
+       const db = client.db("asadmukhtar");
        console.log("Database is connected");
+       return db;
     } catch(error){
         console.log("Connected Database");
     }
 }
-connectDB().catch(console.error);
+//connectDB().catch(console.error);
 // request is for receive data ..
 // response is for send output ...
 app.set('view engine','ejs');
@@ -28,12 +28,35 @@ app.get('/',(req,res)=>{
 app.get('/about',(req,res)=> {
     res.render('about');
 });
-app.get('/products',(req,res) => {
-    res.render('products');
+app.get('/products',async (req,res) => {
+    try {
+        const db = await connectDB(); // Get the database instance
+        const collection = db.collection("products"); // Collection name
+        const products = await collection.find().toArray();
+       // console.log(products);
+        res.render('products',{products});
+    } catch(error){
+        console.log("Error is",error);
+    }
+    //res.render('products');
 });
-app.post('/save',(req,res) => {
-    console.log('Data Got it');
-})
+app.post('/save', async (req,res) =>{
+    const {title,description,price} = req.body;
+    const product = {
+        title,
+        description,
+        price
+    }
+    console.log("Product Details",product);
+    try {
+        const db = await connectDB(); // Get the database instance
+        const collection = db.collection("products"); // Collection name
+        await collection.insertOne(product);
+        res.redirect("/products");
+    } catch(error){
+        console.log("Cant Insert Data Into MongoDB",error);
+    }
+});
 app.get('/contact',(req,res)=> {
     res.render('contact');
 });
